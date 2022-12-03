4bd88cf926174b2d0db7e072622fe4525a49b692
abed <abed-rwx@proton.me>
2022-12-03 22:44:44 +0100

    preparing branch for merge

--- src/main.c
+++ src/main.c
@@ -3,57 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imittous <imittous@student.42.fr>          +#+  +:+       +#+        */
+/*   By: arouzen <arouzen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 09:46:10 by arouzen           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/11/20 12:39:13 by arouzen          ###   ########.fr       */
-=======
-/*   Updated: 2022/11/20 09:19:39 by imittous         ###   ########.fr       */
->>>>>>> 8b98af8ba4fd7818b8fd7f1486f97e214f41ca60
+/*   Updated: 2022/12/03 22:36:15 by arouzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD
-int main(int argc, char *argv[], char **environ)
-{
-	char	*line;
-	t_list	*tok_l;
-	t_list	*tmp;
-
-	(void) argv;
-	(void) argc;
-	//rl_complete('\t', filename_completion_function);
-	while(TRUE)
-	{
-		line  = readline(SHELL_PROMPT);
-		tok_l = lexer(line);
-		if (line && *line)
-		{
-			add_history(line);
-		}
-		tmp = tok_l;
-		if (!unquote(tok_l, line, environ))
-			printf("Quote parse error!\n");
-		printf("token list | ");
-		while (tmp)
-		{
-			printf("-->%d, val:%s ", ((t_token*)tmp->content)->tkn, ((t_token*)tmp->content)->val);
-			tmp = tmp->next;
-		}
-		printf(" |\n");
-		//printf("%d matched\n", match_redi_list(tok_l));
-		printf("%d matched\n", match_pipeline(tok_l));
-		if (ft_lstsize(tok_l) == match_pipeline(tok_l))
-			printf("All matched: SUCCESS\n");
-		else
-			printf("Parse error: FAILURE\n");
-	}
-	return (0);
-}
-=======
 // int main(int argc, char *argv[])
 // {
 // 	char	*line;
@@ -86,4 +44,3 @@ int main(int argc, char *argv[], char **environ)
 // 	}
 // 	return (0);
 // }
->>>>>>> 8b98af8ba4fd7818b8fd7f1486f97e214f41ca60
--- src/quoting.h
+++ src/quoting.h
@@ -6,7 +6,7 @@
 /*   By: arouzen <arouzen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 22:25:14 by arouzen           #+#    #+#             */
-/*   Updated: 2022/12/03 22:30:28 by arouzen          ###   ########.fr       */
+/*   Updated: 2022/12/03 22:35:03 by arouzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@
 int	unquote(t_list *tok_l, char *line, char **environ);
 int	dq_unquote(t_list *tok_l, char *line, char **environ);
 int	sq_unquote(t_list *tok_l, char *line);
-
 
 #endif
